552fce5405ca13d98c62024e7494401a167d058a
goodnews <goodnewsj62@gmail.com>
2024-04-04 05:56:16 +0100

    dark mode

--- new_portfolio/js/general.js
+++ new_portfolio/js/general.js
@@ -18,6 +18,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
     gsap.registerPlugin(ScrollTrigger, TextPlugin)
 
 
+    // typewriter at the hero
 
     const words = ["developer", "engineer", "architect"];
 
@@ -61,7 +62,61 @@ document.addEventListener("DOMContentLoaded", (event) => {
         opacity: 0,
         duration: 0,
         delay: .8
+    });
+
+
+    /* hero slide in effect */
+    const heroTimeline = gsap.timeline({
+        onStart: () => {
+            document.querySelector(".container").style.overflow = "hidden"
+            document.querySelector(".container").style.height = "99svh"
+        },
+        onComplete: () => {
+            document.querySelector(".container").style.overflow = ""
+            document.querySelector(".container").style.height = ""
+        }
+    });
+    Array.from(document.querySelectorAll(".hero > div")).forEach((el, index) => {
+
+        heroTimeline.from(el, {
+            ...(index === 0 ? { x: -200 } : { right: -200 }),
+            opacity: 0,
+            duration: 1
+        }, 0)
+    });
+
+
+    console.log(window.innerHeight)
+
+
+    heroTimeline.from(".services", {
+        y: `${(document.querySelector(".services").getBoundingClientRect().height)}`,
     })
+
+
+    // header effect
+    const headerTimeLine = gsap.timeline({});
+    const headerInfo = document.querySelector("header").getBoundingClientRect();
+
+    headerTimeLine.from("header", {
+        top: `-${headerInfo.height}`,
+        duration: .5
+    })
+
+    Array.from(document.querySelectorAll("#nav__links li")).forEach((el, index) => {
+        headerTimeLine.from(el, {
+            y: `-${(headerInfo.height / 2) + el.offsetHeight}`,
+            duration: .1,
+            delay: (0.1) * index,
+            ease: "expo.out",
+        });
+    });
+
+    headerTimeLine.from(".bulb", {
+        top: -100,
+        delay: 1,
+        duration: .1,
+    });
 
 
 });
